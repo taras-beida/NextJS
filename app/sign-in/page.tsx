99443f2b9fadb55308ca0ea2afb06f204f0ad7f0
Taras <beyda.taras@gmail.com>
2023-08-15 22:15:07 +0300

    Fixes

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -40,6 +40,7 @@ export default function SignIn() {
     <>
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          {/* eslint-disable-next-line @next/next/no-img-element */}
           <img
             className="mx-auto h-10 w-auto"
             src="/next.svg"
