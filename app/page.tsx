bdc40d2f3ff75fcc0b34c18f23bafdc211bf4493
Taras <beyda.taras@gmail.com>
2023-08-15 22:12:33 +0300

    Test commit

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import { getServerSession } from 'next-auth'
 import { options } from '@/app/api/auth/[...nextauth]/options'
 import Link from 'next/link'
 import { redirect } from 'next/navigation'
-import { RedirectType } from 'next/dist/client/components/redirect'
 
 export default async function Home() {
   const session = await getServerSession(options)
@@ -14,7 +13,7 @@ export default async function Home() {
   return (
     <>
       <h1 className="text-3xl font-bold underline">{session.user?.email}</h1>
-      <Link href="/api/auth/signout">Sign Out</Link>
+      <Link href={'/api/auth/signout'}>Sign Out</Link>
       <h2>test</h2>
     </>
   )
